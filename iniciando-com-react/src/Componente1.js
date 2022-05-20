4a2d74c67f18e5406ff5bcc2f5394119e6feb356
c.gentilin <c.gentilin@senff.com.br>
2022-05-20 14:54:06 -0300

    meu primeiro componente funcional react

---
+++ iniciando-com-react/src/Componente1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//meu primeiro componente funcional react
+function Componente1(){
+    return(
+        <h1>Meu primeiro componente funcional react</h1>
+    )
+}
+
+export default Componente1;
--- iniciando-com-react/src/index.js
+++ iniciando-com-react/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Componente1 from './Componente1';
 
 import './index.css'
 
-ReactDOM.render( <App nome="Dougllas" idade={31}  /> , document.getElementById('root'));
+//ReactDOM.render( <App nome="Dougllas" idade={31}  /> , document.getElementById('root'));
+//Renderiza meu primeiro componente funcional react
+ReactDOM.render( <Componente1/> , document.getElementById('root'));
