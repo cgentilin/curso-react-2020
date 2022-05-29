590c0284bf5b11b12d9d6bdc5948ca11282327b3
c.gentilin <c.gentilin@senff.com.br>
2022-05-29 15:39:43 -0300

    componente 2

---
+++ iniciando-com-react/src/Componente2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//meu segundo componete agora de classe
+class Componente2 extends React.Component{
+    render(){
+        return(
+            <h1>Meu primeiro componente de classe react</h1>
+        )
+    }
+}
+
+export default Componente2;
